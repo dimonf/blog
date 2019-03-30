5a27609f3f176f444c67cddeeefbe43ca54b8767
dimonf <camel109@gmail.com>
2019-03-30 05:30:54 +0200

    next chapter

---
+++ flaskr/__init__.py
@@ -0,0 +1,32 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config = None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+            SECRET_KEY = 'dev',
+            DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite')
+            )
+
+    if test_config:
+        app.config.from_mapping(test_config)
+    else:
+        app.config.from_pyfile('config.py', silent=True)
+
+    try:
+        os.makedirs(app.instance_path)
+    except:
+        pass
+
+    #simple page
+    @app.route('/hello')
+    def hello():
+        return 'Hello, world!'
+
+    from . import db
+    db.init_app(app)
+
+    return app
+
